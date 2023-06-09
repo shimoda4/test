93793883d86d4aa96dc6a526a630b9ba738a7480
shimoda-lab <shimodalab.cit@gmail.com>
2023-06-09 20:55:49 +0900

    ddd

---
+++ _study3.sql
@@ -0,0 +1,71 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- ホスト: 127.0.0.1
+-- 生成日時: 2020-06-25 03:05:15
+-- サーバのバージョン： 10.4.11-MariaDB
+-- PHP のバージョン: 7.4.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- データベース: `_study3`
+--
+CREATE DATABASE IF NOT EXISTS `_study3` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `_study3`;
+
+-- --------------------------------------------------------
+
+--
+-- テーブルの構造 `mst_product`
+--
+
+CREATE TABLE `mst_product` (
+  `code` int(11) NOT NULL,
+  `name` varchar(30) COLLATE utf8_unicode_ci NOT NULL,
+  `price` int(11) NOT NULL,
+  `comment` varchar(200) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- テーブルのデータのダンプ `mst_product`
+--
+
+INSERT INTO `mst_product` (`code`, `name`, `price`, `comment`) VALUES
+(1, 'すいか', 100, 'うまいです。'),
+(2, 'きゃべつ', 1000, 'まずい'),
+(14, 'daikon', 2000, 'うまい');
+
+--
+-- ダンプしたテーブルのインデックス
+--
+
+--
+-- テーブルのインデックス `mst_product`
+--
+ALTER TABLE `mst_product`
+  ADD PRIMARY KEY (`code`);
+
+--
+-- ダンプしたテーブルのAUTO_INCREMENT
+--
+
+--
+-- テーブルのAUTO_INCREMENT `mst_product`
+--
+ALTER TABLE `mst_product`
+  MODIFY `code` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
